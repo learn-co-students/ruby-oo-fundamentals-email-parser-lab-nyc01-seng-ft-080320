bd1a0a4f9120e8a981d9bd037b62f912823f5dd7
kain <kaindoneson@gmail.com>
2020-08-03 19:30:20 -0400

    added comments explaining the regex and also a link to the calc

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,9 @@ class EmailAddressParser
     def initialize (email)
         @email = email;
     end
-
+    # The regex /,?\s/ means: match 0 or more commas followed by a whitespace character. So of there is 0 commas it will split
+    #on a space but if there is a comma it will also split on the combo of comma and space. This would fail in an example that has
+    #0 spaces such as "avi@test.com,arel@test.com".
     def parse
         parsed_array = @email.split(/,?\s/);
         parsed_array = parsed_array.uniq;
@@ -20,7 +22,7 @@ end
 #binding.pry;
 
 
-
+# https://rubular.com/ was very helpful too
 # EmailAddressParser.new("avi@test.com, avi@test.com").parse
 # ruby lib/email_parser.rb
 
